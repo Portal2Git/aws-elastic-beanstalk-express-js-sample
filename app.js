9c232f767954dda59583f39327f23c3ae6c7bf4a
Portal2Git <92179469+Portal2Git@users.noreply.github.com>
2021-10-09 22:52:45 +0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('dfsdfsfds fdsfd dss!'));
+app.get('/', (req, res) => res.send('OGO!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
